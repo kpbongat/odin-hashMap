75a95ffec062fda574696ab64675e6b9695b6c17
kpbongat <kpbongat.dev@gmail.com>
2024-08-03 17:15:32 +0800

    Create HashMap class with basic hash function

---
+++ index.js
@@ -0,0 +1,13 @@
+class HashMap {
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % 16;
+    }
+
+    return hashCode;
+  }
+}
